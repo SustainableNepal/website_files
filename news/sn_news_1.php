c95cbccd6bb0d762c99219493c03e6ac651dc7f6
Sahaj Bajracharya <bajras@Sahajs-MacBook-Pro.local>
2015-03-23 20:34:46 -0500

    individual news page set up

--- news/sn_news_1.php
+++ news/sn_news_1.php
@@ -3,7 +3,18 @@
 	include("../multipage_header.php");
 ?>
 
+<section class="individual-news-cont container margin-top50 margin-bottom60">
+	<div class="inner-individual-news-cont">
+		<h6><a href="">NEWS</a></h6>
+		<h1>Mapbox workshop at PARISOMA in SF this Monday</h1>
+		<h5>March 1, 2015</h5>
+		<p>Bacon ipsum dolor amet meatball t-bone venison fatback swine shankle. Ham tongue andouille ground round, kielbasa salami hamburger ham hock beef swine bresaola short ribs alcatra. Alcatra short ribs corned beef spare ribs beef chuck ground round, fatback cupim pork loin pork landjaeger capicola jerky. Strip steak swine doner capicola salami, brisket porchetta pork short ribs. Swine pork loin kielbasa biltong turducken strip steak pastrami cow. Beef ribs brisket fatback, biltong bresaola pastrami leberkas pancetta. Capicola jowl porchetta, cupim corned beef prosciutto tail bacon ground round kevin bresaola tenderloin brisket frankfurter.</p>
+		<p>Bacon ipsum dolor amet meatball t-bone venison fatback swine shankle. Ham tongue andouille ground round, kielbasa salami hamburger ham hock beef swine bresaola short ribs alcatra. Alcatra short ribs corned beef spare ribs beef chuck ground round, fatback cupim pork loin pork landjaeger capicola jerky. Strip steak swine doner capicola salami, brisket porchetta pork short ribs. Swine pork loin kielbasa biltong turducken strip steak pastrami cow. Beef ribs brisket fatback, biltong bresaola pastrami leberkas pancetta. Capicola jowl porchetta, cupim corned beef prosciutto tail bacon ground round kevin bresaola tenderloin brisket frankfurter.</p>
+		<p>Bacon ipsum dolor amet meatball t-bone venison fatback swine shankle. Ham tongue andouille ground round, kielbasa salami hamburger ham hock beef swine bresaola short ribs alcatra. Alcatra short ribs corned beef spare ribs beef chuck ground round, fatback cupim pork loin pork landjaeger capicola jerky. Strip steak swine doner capicola salami, brisket porchetta pork short ribs. Swine pork loin kielbasa biltong turducken strip steak pastrami cow. Beef ribs brisket fatback, biltong bresaola pastrami leberkas pancetta. Capicola jowl porchetta, cupim corned beef prosciutto tail bacon ground round kevin bresaola tenderloin brisket frankfurter.</p>
+		<p>Bacon ipsum dolor amet meatball t-bone venison fatback swine shankle. Ham tongue andouille ground round, kielbasa salami hamburger ham hock beef swine bresaola short ribs alcatra. Alcatra short ribs corned beef spare ribs beef chuck ground round, fatback cupim pork loin pork landjaeger capicola jerky. Strip steak swine doner capicola salami, brisket porchetta pork short ribs. Swine pork loin kielbasa biltong turducken strip steak pastrami cow. Beef ribs brisket fatback, biltong bresaola pastrami leberkas pancetta. Capicola jowl porchetta, cupim corned beef prosciutto tail bacon ground round kevin bresaola tenderloin brisket frankfurter.</p>
 
+	</div>	
+</section>
 
 <?php include("../sn_footer.php") ?>
 <?php include("../footer.php") ?>
