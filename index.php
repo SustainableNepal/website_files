2e780381a0c39669014bef735f5602e4f8e4fcb9
Sahaj Bajracharya <sahajnb@gmail.com>
2015-04-13 15:55:00 -0500

    fix

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php include("header.php") ?>
 
 <section id="home" class="parallax-bg">
-    <div class="parallax-overlay"></div> 
-    <div class="home-content" style="display: none;">
-        <div class="container">
-            <div class="slide-logo text-center wow fadeIn" data-wow-delay="200ms"><img class="image-responsive" src="images/Logo_SustainNepal_white.png" style="width: 100px; height: 155px;"></div>
-            <div class="main-flex-slider wow fadeIn" data-wow-delay="400ms">
-                <h1 class="rotate">Environmental Sustainability, Economic Sustainability, Social Sustainability</h1>
-            </div> 
-            <h2 class="slide-btm-text wow fadeIn" data-wow-delay="600ms">For a green future in nepal.</h2>
-            <div class="text-center scrollto wow animated fadeInUp" data-wow-delay="400ms">
-                <a id="about-scroll" href="#about" class="btn btn-default">Get Started<span class="icon-down" style="margin-left: 10px;"></span></a>
+    <div class="parallax-overlay"></div>
+        <div style="display: table;"> 
+            <div class="home-content" style="display: none;">
+                <div class="container">
+                    <div class="slide-logo text-center wow fadeIn" data-wow-delay="200ms"><img class="image-responsive" src="images/Logo_SustainNepal_white.png" style="width: 100px; height: 155px;"></div>
+                    <div class="main-flex-slider wow fadeIn" data-wow-delay="400ms">
+                        <h1 class="rotate">Environmental Sustainability, Economic Sustainability, Social Sustainability</h1>
+                    </div> 
+                    <h2 class="slide-btm-text wow fadeIn" data-wow-delay="600ms">For a green future in nepal.</h2>
+                    <div class="text-center scrollto wow animated fadeInUp" data-wow-delay="400ms">
+                        <a id="about-scroll" href="#about" class="btn btn-default">Get Started<span class="icon-down" style="margin-left: 10px;"></span></a>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 </section>
 
 <section id="navigation">
