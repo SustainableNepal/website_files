8eec5f6ed3fdda3a793ae4eeb1210c2cea3df816
Sahaj Bajracharya <bajras@Sahajs-MacBook-Pro.local>
2015-04-13 20:43:56 -0500

    Adding Learn More Text

--- project.php
+++ project.php
@@ -3,7 +3,7 @@
         <img src="images/slide-bg.jpg" alt="img04"/>
         <figcaption>
             <div>Forest <span>Himalaya</span></div>
-            <p>Forest Himalaya aims to improve environmental conditions in the Himalayas by promoting reforestation and uplifting the livelihoods of the people through ecological practices.<br/><span class="icon-rightarrow"></span></p>
+            <p>Forest Himalaya aims to improve environmental conditions in the Himalayas by promoting reforestation and uplifting the livelihoods of the people through ecological practices.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
             <a href="foresthimalaya.php">View more</a>
         </figcaption>           
     </figure>
@@ -11,7 +11,7 @@
         <img src="images/world_map.jpg" alt="img04"/>
         <figcaption>
             <div>Sustainable <span>Map</span></div>
-            <p>Sustainable Map is a dynamic map that allows users to add their point location to the global map to showcase their sustainable examples and learn from other projects.<br/><span class="icon-rightarrow"></span></p>
+            <p>Sustainable Map is a dynamic map that allows users to add their point location to the global map to showcase their sustainable examples and learn from other projects.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
             <a href="sustainablemap.php">View more</a>
         </figcaption>           
     </figure>
@@ -19,7 +19,7 @@
         <img src="images/Paragliding_Nepal.jpg" alt="img04"/>
         <figcaption>
             <div>Fly <span>Green</span></div>
-            <p>You have an option to make your flying eco-friendly. Plant the number of trees that would compensate the carbon footprints you created during your flight.<br/><span class="icon-rightarrow"></span></p>
+            <p>You have an option to make your flying eco-friendly. Plant the number of trees that would compensate the carbon footprints you created during your flight.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
             <a href="flygreen.php">View more</a>
         </figcaption>           
     </figure>
@@ -27,7 +27,7 @@
         <img src="images/greenliving.jpg" alt="img04"/>
         <figcaption>
             <div>Green <span>Living</span></div>
-            <p>Here you can find tips for adopting more sustainable and healthy lifestyle - Simple change in our everyday lives can help in slowing climate change!<br/><span class="icon-rightarrow"></span></p>
+            <p>Here you can find tips for adopting more sustainable and healthy lifestyle - Simple change in our everyday lives can help in slowing climate change!<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
             <a href="greenliving.php">View more</a>
         </figcaption>           
     </figure>
@@ -35,7 +35,7 @@
         <img src="images/exchange.jpg" alt="img04"/>
         <figcaption>
             <div>Sustainable<span> Exchange</span></div>
-            <p>Learning exchange programs related to sustainability are organized to experience and learn sustainable practices in different culture and with international perspectives.<br/><span class="icon-rightarrow"></span></p>
+            <p>Learning exchange programs related to sustainability are organized to experience and learn sustainable practices in different culture and with international perspectives.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
             <a href="sustainableexchange.php">View more</a>
         </figcaption>           
     </figure>
@@ -43,7 +43,7 @@
         <img src="images/nepal123.jpg" alt="img04"/>
         <figcaption>
             <div>Research<span> Nepal</span></div>
-            <p>Sustainable Nepal focuses on the priorities of Nepalese people living in Nepal and rest of the world and help build a collective vision to plan development agendas to make Nepal a better place to live in.<br/><span class="icon-rightarrow"></span></p>
+            <p>Sustainable Nepal focuses on the priorities of Nepalese people living in Nepal and rest of the world and help build a collective vision to plan development agendas to make Nepal a better place to live in.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
             <a href="mynepal.php">View more</a>
         </figcaption>           
     </figure>
