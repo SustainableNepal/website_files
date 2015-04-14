2dcd3bc07393cb1b0f998cda285f7cfecfbaf6b0
Sahaj Bajracharya <bajras@Sahajs-MacBook-Pro.local>
2015-04-13 23:25:31 -0500

    Revert "lazy loading images"
    This reverts commit f00a95132d26792d81ef304f576e857a3155460d.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script async src="<?php dirname(__FILE__) ?>/js/build/production.js"></script>
+    <script async src="<?php dirname(__FILE__) ?>/js/build/production.min.js"></script>
   </body>
 </html>
