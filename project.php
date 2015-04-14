298d9795b44de3d325a35f130dd1d9b942f9b3a6
Sahaj Bajracharya <bajras@Sahajs-MacBook-Pro.local>
2015-04-13 23:05:26 -0500

    lazy load

--- project.php
+++ project.php
@@ -1,6 +1,6 @@
 <div class="grid">
     <figure class="effect-layla">
-        <img src="images/slide-bg.jpg" alt="img04"/>
+        <img data-original="images/slide-bg.jpg" class="lazy"/>
         <figcaption>
             <div>Forest <span>Himalaya</span></div>
             <p>Forest Himalaya aims to improve environmental conditions in the Himalayas by promoting reforestation and uplifting the livelihoods of the people through ecological practices.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
@@ -8,7 +8,7 @@
         </figcaption>           
     </figure>
     <figure class="effect-layla">
-        <img src="images/world_map.jpg" alt="img04"/>
+        <img data-original="images/world_map.jpg" class="lazy"/>
         <figcaption>
             <div>Sustainable <span>Map</span></div>
             <p>Sustainable Map is a dynamic map that allows users to add their point location to the global map to showcase their sustainable examples and learn from other projects.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
@@ -16,7 +16,7 @@
         </figcaption>           
     </figure>
     <figure class="effect-layla">
-        <img src="images/Paragliding_Nepal.jpg" alt="img04"/>
+        <img data-original="images/Paragliding_Nepal.jpg" class="lazy"/>
         <figcaption>
             <div>Fly <span>Green</span></div>
             <p>You have an option to make your flying eco-friendly. Plant the number of trees that would compensate the carbon footprints you created during your flight.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
@@ -24,7 +24,7 @@
         </figcaption>           
     </figure>
     <figure class="effect-layla">
-        <img src="images/greenliving.jpg" alt="img04"/>
+        <img data-original="images/greenliving.jpg" class="lazy"/>
         <figcaption>
             <div>Green <span>Living</span></div>
             <p>Here you can find tips for adopting more sustainable and healthy lifestyle - Simple change in our everyday lives can help in slowing climate change!<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
@@ -32,7 +32,7 @@
         </figcaption>           
     </figure>
     <figure class="effect-layla">
-        <img src="images/exchange.jpg" alt="img04"/>
+        <img data-original="images/exchange.jpg" class="lazy"/>
         <figcaption>
             <div>Sustainable<span> Exchange</span></div>
             <p>Learning exchange programs related to sustainability are organized to experience and learn sustainable practices in different culture and with international perspectives.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
@@ -40,7 +40,7 @@
         </figcaption>           
     </figure>
     <figure class="effect-layla">
-        <img src="images/nepal123.jpg" alt="img04"/>
+        <img data-original="images/nepal123.jpg" class="lazy"/>
         <figcaption>
             <div>Research<span> Nepal</span></div>
             <p>Sustainable Nepal focuses on the priorities of Nepalese people living in Nepal and rest of the world and help build a collective vision to plan development agendas to make Nepal a better place to live in.<br/><br/><span class="text-tiny" style="font-size: 12px;">Learn More</span><span class="icon-rightarrow"></span></p>
