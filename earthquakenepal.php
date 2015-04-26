959a423b717fc84f307c192f6e51bbfab5070dc5
Sahaj Bajracharya <bajras@Sahajs-MacBook-Pro.local>
2015-04-26 15:35:42 -0500

    change

--- earthquakenepal.php
+++ earthquakenepal.php
@@ -11,7 +11,7 @@
                     </div> 
                     <h2 class="slide-btm-text wow fadeIn" data-wow-delay="600ms" style="margin-top: 15px;">MAGNITUDE 7.9</h2>
                     <div class="text-center scrollto wow animated fadeInUp" data-wow-delay="400ms">
-                        <a id="about-scroll" href="#about" class="btn btn-default earthquake">Read More<span class="icon-down" style="margin-left: 10px;"></span></a>
+                        <a id="about-scroll" href="#about" class="btn btn-default earthquake" style=" background: #e04515; border-color: #e04515; color: #fff;">Read More<span class="icon-down" style="margin-left: 10px;"></span></a>
                     </div>
                 </div>
             </div>
@@ -21,8 +21,8 @@
 <section id="about" style="margin-top: 0; padding-top: 0;">
     <div class="container">        
         <div class="margin-top80 margin-bottom80 text-center section-heading wow animated fadeInUp" data-wow-duration="700ms" data-wow-delay="300ms">
-            <div class="label-text margin-bottom25">Our mission is to accelerate the transition and growth of a new green economic paradigm in Nepal to achieve goals of sustainable development.</div>
-            <p class="text-small about-paragraph">Sustainable Nepal was established on the occasion of World Environment Day - June 5, 2012. This organization aims to foster ecological, economical and social sustainable development of Nepal though projects that combines knowledge and action, both on national and international levels, to help create conditions for a sustainable future.</p>
+            <div class="label-text margin-bottom25">On 25 April, 11:41 local time, an earthquake with of 7.9 magnitude hit Nepal near the capital city of Kathmandu killing thousands of people. There have been several aftershocks with magnitudes ranging from 4.2–6.7 till now.</div>
+            <!-- <p class="text-small about-paragraph">Sustainable Nepal was established on the occasion of World Environment Day - June 5, 2012. This organization aims to foster ecological, economical and social sustainable development of Nepal though projects that combines knowledge and action, both on national and international levels, to help create conditions for a sustainable future.</p> -->
         </div>
     </div>
 </section>
@@ -59,20 +59,28 @@
     ?>
 </div>
 
-<div id="donate">
-    <div class="container text-center">
-        <h4 class="margin-bottom25">NEPAL EARTHQUAKE MAPPING</h4>
-        <p>Consider donating to our projects. Your gift will help our organization<br/>to mobilize our resources for a better Nepal.</p>
-        <a href="http://www.sustainablemap.org/earthquakenepal" target="_blank" class="btn btn-default donate">VIEW MAP</a>
+<section id="about" style="margin-top: 0; padding-top: 0; background-color: #ffffff;">
+    <div class="container">        
+        <div class="margin-top80 margin-bottom80 text-center section-heading wow animated fadeInUp" data-wow-duration="700ms" data-wow-delay="300ms">
+            <div class="label-text margin-bottom25">NEPAL EARTHQUAKE MAPPING</div>
+            <p class="text-small about-paragraph">Sustainable Nepal has developed an Earthquake mapping tool that helps users to post information (both text and pictures) about earthquake impacts as well activities related to the earthquake. During the process of recovering, this tool can be useful for collaborations – visualising the on-going activities and spreading information among local actors and to the public.</p>
+        </div>
+    </div>
+</section>
+
+<div id="donate" style="background-image:url(images/map.png); background-position: 0% 56%; padding-top: 120px; padding-bottom: 120px;">
+    <div class="parallax-overlay" style="margin-top: -120px; height: 340px;"></div>
+    <div class="container text-center" style=" color: #ffffff;; position: relative;">
+        <a href="http://www.sustainablemap.org/earthquakenepal" target="_blank" class="map btn btn-default" style=" background: #e04515; border-color: #e04515; color: #fff;">VIEW MAP</a>
                 <!-- <a href="" class="btn btn-default donate">DONATE</a> -->
         </form>
     </div>
 </div>
-<div id="donate" style="background-color: #ffffff;">
+<div id="donate">
     <div class="container text-center">
         <h4 class="margin-bottom25">STAY STRONG NEPAL</h4>
         <p>Your gift will fund the most urgent needs for earthquake survivors in Nepal.</p>
-        <a href="http://www.redcross.se/teman/nepal/" target="_blank" class="btn btn-default donate">DONATE</a>
+        <a href="http://www.redcross.se/teman/nepal/" target="_blank" class="btn btn-default map" style=" background: #e04515; border-color: #e04515; color: #fff;">DONATE</a>
                 <!-- <a href="" class="btn btn-default donate">DONATE</a> -->
         </form>
     </div>
